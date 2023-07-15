e192c02cf070bb7fdaad46692a0a702c024a6ecd
Asif <asifpatel068@gmail.com>
2023-07-15 20:46:58 +0530

    completed

---
+++ prob1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      
+      <h1>Hello World!</h1>
+      <h1>Counter Application</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ prob1/src/Counter.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement} disabled={count === 0}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
